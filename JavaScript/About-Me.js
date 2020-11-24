690cb275fe75c50d0ba6b3528f2ebbb100d2656a
WarpedSinner <35859863+WarpedSinner@users.noreply.github.com>
2020-11-24 01:20:29 -0500

    Added my own buttons
    Added my own buttons along witht he css style sheet to go along with it, buttons have javascript that opens there text below them when clicked and closes it when clicked again

---
+++ JavaScript/About-Me.js
@@ -0,0 +1,29 @@
+var SlideOpen = [];
+InitSlides();
+
+function InitSlides()
+{
+	var i;
+	var slides = document.getElementsByClassName("MenuSlides");
+	for(i = 0; i < slides.length; i++)
+	{
+		SlideOpen.push(false);
+	}
+}
+
+function SlideStateUpdate(n)
+{
+	var i;
+	var slides = document.getElementsByClassName("MenuSlides");
+	if(SlideOpen[(n-1)] == false)
+	{
+		slides[n-1].style.display = "block";
+		SlideOpen[n-1] = true;
+	}else if(SlideOpen[(n-1)] == true)
+	{
+		slides[n-1].style.display = "none";
+		SlideOpen[n-1] = false;
+	}
+	
+	console.log(SlideOpen);
+}
