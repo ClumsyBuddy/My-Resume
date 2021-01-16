447294e29397cc2ada0693e24a1349642f7f0d78
WarpedSinner <35859863+WarpedSinner@users.noreply.github.com>
2021-01-16 01:08:03 -0500

    Large additions to price page

--- JavaScript/PriceMaker.js
+++ JavaScript/PriceMaker.js
@@ -1,18 +1,29 @@
 BaseCost = 70;
 FinalCost = 0;
 
+PricePerPage = 25;
+ResponsivePageCost = 35;
+CustomLayoutPrice = 45;
 
 DisplayFinalCost(BaseCost)
 
 function CalcFinalCost() {
+    FinalCost = BaseCost;
+
+
     var PageNumber = document.getElementById("PGNUM");
     MaxClamp(PageNumber);
-    PageCost = (PageNumber.valueAsNumber - 1) * 5;
+    PageCost = (PageNumber.valueAsNumber - 1) * PricePerPage;
+
+    if (IsCheckMarked("RPAGE")) {
+        FinalCost += ResponsivePageCost;
+    }
+    if (IsCheckMarked("CustomLayout")) {
+        FinalCost += CustomLayoutPrice;
+    }
 
 
-
-
-    FinalCost = PageCost + BaseCost;
+    FinalCost += PageCost;
     DisplayFinalCost(FinalCost);
 }
 
@@ -24,7 +35,14 @@ function DisplayFinalCost(Cost) {
 
 
 
-
+function IsCheckMarked(id) {
+    var thiselement = document.getElementById(id);
+    if (thiselement.checked == true) {
+        return true
+    } else {
+        return false
+    }
+}
 
 
 
