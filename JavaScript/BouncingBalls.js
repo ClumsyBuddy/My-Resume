366119a8c404c3dd22c3e4c1403d2e02b6bdcacd
WarpedSinner <35859863+WarpedSinner@users.noreply.github.com>
2021-01-18 15:11:24 -0500

    Mouse position, clear screen

--- JavaScript/BouncingBalls.js
+++ JavaScript/BouncingBalls.js
@@ -1,7 +1,7 @@
-var doc = document.getElementById("PricingCanvas");
-doc.width = window.innerWidth * 0.90;
-doc.height = window.innerHeight * 0.90;
-var cdoc = doc.getContext("2d");
+const canvas = /** @type {HTMLCanvasElement} */ (document.getElementById('PricingCanvas'));
+canvas.width = window.innerWidth * 0.90;
+canvas.height = window.innerHeight * 0.90;
+var cdoc = canvas.getContext("2d");
 
 /** @type {CanvasRenderingContext2D} */
 
@@ -14,24 +14,33 @@ var Radius = 10;
 var BallX = 50;
 var BallY = 50;
 
-DrawRect();
 
-
+function Rect(canvas, e) {
+    Clear();
+    var pos = getCursorPosition(canvas, e);
+    cdoc.fillRect(pos.x, pos.y, 50, 50);
+    cdoc.fill();
+}
 
 function DrawRect() {
     for (var i = 0; i < RectAmount; i++) {
-        var x = (Math.random() * doc.width) + 1
-        var y = (Math.random() * doc.height) + 1
+        var x = (Math.random() * canvas.width) + 1
+        var y = (Math.random() * canvas.height) + 1
         cdoc.fillRect(x, y, 50, 50);
     }
 }
 
-function DrawBall(canvas, event) {
+function Clear() {
+    cdoc.clearRect(0, 0, canvas.width, canvas.height);
+}
 
+function DrawBall(canvas, event) {
+    //Clear();
     var pos = getCursorPosition(canvas, event)
     console.log("x: " + pos.x + " y: " + pos.y)
+    cdoc.beginPath();
     cdoc.arc(pos.x, pos.y, Radius, 0, 2 * Math.PI);
-    cdoc.fill()
+    cdoc.fill();
 }
 
 
@@ -44,7 +53,6 @@ function getCursorPosition(canvas, event) {
     return pos;
 }
 
-const canvas = document.querySelector('canvas')
 canvas.addEventListener('mousedown', function(e) {
     DrawBall(canvas, e);
 })
