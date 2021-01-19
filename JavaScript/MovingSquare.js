022f761de3927120198decd2c6f627a5272f76ad
WarpedSinner <35859863+WarpedSinner@users.noreply.github.com>
2021-01-18 19:02:51 -0500

    Added another cube along with gravity and mouseposition

--- JavaScript/MovingSquare.js
+++ JavaScript/MovingSquare.js
@@ -1,7 +1,10 @@
 var myGamePiece;
+var OtherRect;
+
 
 function startGame() {
     myGamePiece = new component(30, 30, "red", 225, 225);
+    OtherRect = new Rect(30, 30, "blue", 225, 225);
     myGameArea.start();
 }
 
@@ -20,6 +23,14 @@ var myGameArea = {
         })
         window.addEventListener('keyup', function(e) {
             myGameArea.keys[e.keyCode] = (e.type == "keydown");
+        })
+        window.addEventListener('mousemove', function(e) {
+            const rect = myGameArea.canvas.getBoundingClientRect()
+            myGameArea.mousePos = { x: e.clientX - rect.left, y: e.clientY - rect.top };
+        })
+        window.addEventListener('resize', function(e) {
+            myGameArea.canvas.width = window.innerWidth * 0.90;
+            myGameArea.canvas.height = window.innerHeight * 0.90;
         })
     },
     stop: function() {
@@ -41,8 +52,9 @@ var myGameArea = {
         if (Entity.y > myGameArea.canvas.height) {
             Entity.y = myGameArea.canvas.height;
         }
-    }
+    },
 }
+
 
 function component(width, height, color, x, y, type) {
 
@@ -68,14 +80,44 @@ function component(width, height, color, x, y, type) {
         this.x += this.speed * Math.sin(this.angle);
         this.y -= this.speed * Math.cos(this.angle);
     }
-    this.WindowResize = function() {
-        myGameArea.canvas.width = window.innerWidth * 0.90;
-        myGameArea.canvas.height = window.innerHeight * 0.90;
+}
+
+function Rect(width, height, color, x, y, type) {
+
+    this.type = type;
+    this.width = width;
+    this.height = height;
+    this.speed = 0;
+    this.angle = 0;
+    this.moveAngle = 0;
+    this.gravity = 5;
+    this.velocity = { x: 0, y: 0 };
+    this.x = x;
+    this.y = y;
+    this.update = function() {
+        ctx = myGameArea.context;
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(this.angle);
+        ctx.fillStyle = color;
+        ctx.fillRect(this.width / -2, this.height / -2, this.width, this.height);
+        ctx.restore();
+    }
+    this.newPos = function() {
+        this.angle += this.moveAngle * Math.PI / 180;
+        this.x += this.speed * Math.sin(this.angle);
+        this.y -= this.speed * Math.cos(this.angle);
+        this.y += this.gravity;
+    }
+    this.PhysicsUpdate = function() {
+        this.velocity.x = MinMaxClamp(this.velocity.x, -5, 5);
+        this.velocity.y = MinMaxClamp(this.velocity.y, -5, 5);
     }
 }
 
+
+
 function updateGameArea() {
-    myGamePiece.WindowResize();
     myGameArea.clear();
     myGamePiece.moveAngle = 0;
     myGamePiece.speed = 0;
@@ -83,8 +125,30 @@ function updateGameArea() {
     if (myGameArea.keys && myGameArea.keys[68] || myGameArea.keys && myGameArea.keys[39]) { myGamePiece.moveAngle = 3; }
     if (myGameArea.keys && myGameArea.keys[87] || myGameArea.keys && myGameArea.keys[38]) { myGamePiece.speed = 5; }
     if (myGameArea.keys && myGameArea.keys[83] || myGameArea.keys && myGameArea.keys[40]) { myGamePiece.speed = -5; }
+    //PlayerPiece
     myGamePiece.newPos();
     myGameArea.Boundaries(myGamePiece);
     myGamePiece.update();
+    //Testing piece
+    OtherRect.newPos();
+    myGameArea.Boundaries(OtherRect);
+    OtherRect.update();
 
+    //console.log(myGameArea.mousePos.x)
+}
+
+
+
+
+
+
+
+function MinMaxClamp(Value, Min = '', Max = '') {
+    if (Value > Max) {
+        return Max;
+    }
+    if (Value < Min) {
+        return Min;
+    }
+    return Value;
 }
