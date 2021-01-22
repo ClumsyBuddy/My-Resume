d44d2925dd88594425bd06337d04b0544f9ab316
WarpedSinner <35859863+WarpedSinner@users.noreply.github.com>
2021-01-21 19:26:16 -0500

    Added ability to load js dynamically

--- JavaScript/Loader.js
+++ JavaScript/Loader.js
@@ -1,17 +1,17 @@
 function AddScript(ScriptName) {
     var DifferentScripts = ScriptName.split(',');
+    var Path = "../JavaScript/";
+    var ScriptAmount = DifferentScripts.length;
+    var script_tag = [];
     console.log(DifferentScripts);
+    for (var i = 0; i < DifferentScripts.length; i++) {
+        script_tag.push(document.createElement("script"));
+    }
 
-}
+    for (var j = 0; j < DifferentScripts.length; j++) {
+        script_tag[j].setAttribute('src', Path + DifferentScripts[j]);
 
-
-
-
-function InsertScript() {
-
-
-
-
-
-
+        document.getElementById("LoadedScripts").appendChild(script_tag[j]);
+    }
+    console.log(script_tag);
 }
